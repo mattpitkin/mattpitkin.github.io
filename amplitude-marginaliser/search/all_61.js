92cdfb982d170de38a1eabfe7c24fea81b90a0a4
Matthew Pitkin <matthew.pitkin@ligo.org>
2018-02-02 17:28:26 +0000

    Re-add amplitude marginaliser docs

---
+++ amplitude-marginaliser/search/all_61.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['am_5fln2pi',['AM_LN2PI',['../ampmarginaliser_8h.html#aa48dcc9cf0463d91b0162b4ee319cc60',1,'ampmarginaliser.h']]],
+  ['am_5flnpi_5f2',['AM_LNPI_2',['../ampmarginaliser_8h.html#a719d196420a571f6eebdc80e375fae12',1,'ampmarginaliser.h']]],
+  ['am_5fsquare',['AM_SQUARE',['../ampmarginaliser_8h.html#ab6f7bd4382472f906821526216a9939f',1,'ampmarginaliser.h']]],
+  ['ampmarginaliser_2ec',['ampmarginaliser.c',['../ampmarginaliser_8c.html',1,'']]],
+  ['ampmarginaliser_2eh',['ampmarginaliser.h',['../ampmarginaliser_8h.html',1,'']]]
+];
---
+++ amplitude-marginaliser/search/files_61.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['ampmarginaliser_2ec',['ampmarginaliser.c',['../ampmarginaliser_8c.html',1,'']]],
+  ['ampmarginaliser_2eh',['ampmarginaliser.h',['../ampmarginaliser_8h.html',1,'']]]
+];
